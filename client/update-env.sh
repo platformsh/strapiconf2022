7f5f4165005743600b6cb01171c601d25ceffb83
chadcarlson <chad.carlson@platform.sh>
2022-02-16 10:13:12 -0500

    Update start frontend.

--- client/update-env.sh
+++ client/update-env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 API_URL_UNTRIMMED=$(echo $PLATFORM_ROUTES | base64 --decode | jq -r 'to_entries[] | select (.value.id == "api") | .key')
-printf "NEXT_PUBLIC_API_URL=${API_URL_UNTRIMMED::-1}" > .env
+printf "NEXT_PUBLIC_API_URL=${API_URL_UNTRIMMED::-1}\n" > .env
 printf "PREVIEW_SECRET=mysecret" >> .env
